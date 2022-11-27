a5b436856907f28c7a9bd6d81cd55a9e21299463
Marta Sousa <marta.sousa@gmail.com>
2022-11-27 09:03:59 -0300

    passando fakeData para server (conectando front com back)

--- client/src/pages/BlogList.js
+++ client/src/pages/BlogList.js
@@ -1,25 +1,40 @@
-import React, { useEffect } from 'react';
-import FakeData from '../utils/FakeData';
+import React, { useState, useEffect } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import BlogCard from '../components/BlogCard';
 import SearchBar from '../components/SearchBar';
 
 const BlogListPage = () => {
+  const [blogPosts, setBlogPosts] = useState(null);
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.posts);
+        setBlogPosts(data.posts);
+      });
+  }, []);
 
   return (
     <Container>
-      <section>
-        <SearchBar />
-      </section>
-      <section>
-        <h4>Exibindo todos os posts</h4>
+      {!blogPosts ? (
+        <div>Carregando</div>
+      ) : (
         <div>
-          {FakeData.map((each) => (
-            <BlogCard key={each.id} blogItem={each} />
-          ))}
+          <section>
+            <SearchBar />
+          </section>
+          <section>
+            <h4>Exibindo todos os posts</h4>
+            <div>
+              {blogPosts.map((each) => (
+                <BlogCard key={each.id} blogItem={each} />
+              ))}
+            </div>
+          </section>
         </div>
-      </section>
+      )}
     </Container>
   );
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,32 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api', (req, res) => {
+  res.json({
+    posts: [
+      {
+        id: 1,
+        title: 'Crie uma rotina de banho para dormir',
+        author: 'Persono',
+        date: '12/10/2022',
+        description:
+          'O banho é um ótimo companheiro para nos acordar pelas manhãs, sobretudo quando começamos o dia bem cedo. Mas ele também pode ser perfeito para encerrar o dia, nos preparando para dormir.',
+        category: 'SONO E CIÊNCIA',
+      },
+      {
+        id: 2,
+        title: 'Crie uma rotina de banho para dormir',
+        author: 'Persono',
+        date: '12/10/2022',
+        description:
+          'O banho é um ótimo companheiro para nos acordar pelas manhãs, sobretudo quando começamos o dia bem cedo. Mas ele também pode ser perfeito para encerrar o dia, nos preparando para dormir.',
+        category: 'SONO E CIÊNCIA',
+      },
+    ],
+  });
+});
 
 // Inicia backend - escutando porta 5000
-app.listen(5000, () => {console.log('Servidor iniciado na porta 5000');})
+app.listen(5000, () => {
+  console.log('Servidor iniciado na porta 5000');
+});
